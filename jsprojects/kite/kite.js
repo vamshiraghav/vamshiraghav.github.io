6f198f8a0701cc80992a7c398ae4a8349e26fd43
vamshiraghav <vamshiraghava@gmail.com>
2020-03-11 16:29:28 +0530

    background color changed and js refactor abit

--- jsprojects/kite/kite.js
+++ jsprojects/kite/kite.js
@@ -1,4 +1,22 @@
 
+var kid="kitepath";
+var tid="thread";
+var agz=0;
+var agx=0;
+var agy=0;
+var cx = 800;
+var cy = 700;
+var cz= 100;
+var px=800;
+var py=600;
+var angflag=1;
+var xaflag=1;
+var yaflag=1;
+var angswing=10;
+var swingflag=true;
+var svg=document.getElementById("svgbox");
+
+
 function paintkite(cx,cy,w,h,angle,id){
     //cx,cy center of kite
     // angle is angle of kite wrt to center line anticlockwise direction
@@ -16,6 +34,7 @@ function paintkite(cx,cy,w,h,angle,id){
     //path.setAttributeNS(null,"d","M"+tailx+" "+taily+" "+topx+" "+topy+" "+rx+" "+ry+" "+tailx+" "+taily+" "+lx+" "+ly+" "+topx+" "+topy+"Z M"+lx+" "+ly+" Q"+topx+" "+topy+" "+rx+" "+ry+"Z M"+tailx+" "+taily+" t -10 10 10 20 -10 30 10 40");
     //alert("M"+tailx+" "+taily+" "+topx+" "+topy+" "+rx+" "+ry+" "+tailx+" "+taily+" "+lx+" "+ly+" "+topx+" "+topy);
 }
+
 function paintkite3d(cx,cy,cz,w,h,angle,xang,yang,id,px,py,scale,sw,sh,sd){
     //cx,cy center of kite
     // angle is angle of kite wrt to center line anticlockwise direction
@@ -54,6 +73,7 @@ function paintkite3d(cx,cy,cz,w,h,angle,xang,yang,id,px,py,scale,sw,sh,sd){
     //path.setAttributeNS(null,"d","M"+tailx+" "+taily+" "+topx+" "+topy+" "+rx+" "+ry+" "+tailx+" "+taily+" "+lx+" "+ly+" "+topx+" "+topy+"Z M"+lx+" "+ly+" Q"+topx+" "+topy+" "+rx+" "+ry+"Z M"+tailx+" "+taily+" t -10 10 10 20 -10 30 10 40");
     //alert("M"+tailx+" "+taily+" "+topx+" "+topy+" "+rx+" "+ry+" "+tailx+" "+taily+" "+lx+" "+ly+" "+topx+" "+topy);
 }
+
 function xyzper(x,y,z,px,py,scale,w,h,d){
         var dy=Math.round(d/2);
         var dx=Math.round(w*dy/h);
@@ -61,6 +81,7 @@ function xyzper(x,y,z,px,py,scale,w,h,d){
         var newy=Math.round(((py-dy)*z/scale)+y*(w*(scale-z)+2*z*dy)/(w*scale));
         return [newx,newy];
 }
+
 function drawperspect(w,h,d,cx,cy,scale,mar){
     // w-width of svg,h-height of svg, d-minimum depth of longest perspective
     //(cx,cy) -center of perspective , scale- scale down to longest perspect
@@ -84,22 +105,7 @@ function drawperspect(w,h,d,cx,cy,scale,mar){
     //img1.setAttributeNS(null,"height",(dy*2));
     
 }
-paintkite(100,100,100,100,0,"kitepath");
-var kid="kitepath";
-var tid="thread";
-var agz=0;
-var agx=0;
-var agy=0;
-var cx = 800;
-var cy = 700;
-var cz= 100;
-var px=800;
-var py=600;
-var angflag=1;
-var xaflag=1;
-var yaflag=1;
-var angswing=10;
-var swingflag=true;
+
 function krot(){
     
     if(cy>750){
@@ -149,9 +155,9 @@ function krot(){
     }
 
 }
-//drawperspect(1200,600,50,600,300,1000,10);
-//paintkite3d(800,600,100,100,100,0,0,90,kid,600,300,1000,1200,600,50);
-var svg=document.getElementById("svgbox");
+
+
+
 svg.onmousedown = function(event) {
 
     //px = event.clientX;
@@ -181,8 +187,8 @@ svg.onmousedown = function(event) {
     };
   
   };
-  
-  document.onkeydown = checkKey;
+
+document.onkeydown = checkKey;
 
 function checkKey(e) {
 
@@ -190,8 +196,8 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
         //alert("up button");
-        cx=cx-Math.round(1*Math.sin(agz*Math.PI/180));
-        cy=cy-Math.round(2*Math.cos(agz*Math.PI/180));
+        cx=cx-Math.round(2*Math.sin(agz*Math.PI/180));
+        cy=cy-Math.round(4*Math.cos(agz*Math.PI/180));
         cz=cz+3;
     }
     else if (e.keyCode == '40') {
@@ -233,5 +239,5 @@ function checkKey(e) {
      }
 
 }
-  
+
 setInterval(krot,40);
